f790c82640f525cb27821704ad4fb0c2928c0afd
yoann <yoann.ochietti@gmail.com>
2024-10-03 12:53:53 -0400

    Create function to return random choice

--- rps.js
+++ rps.js
@@ -1,1 +1,12 @@
 console.log("hello world");
+
+function getComputerChoice(){
+    let choice = Math.floor(Math.random() * 3);
+    if(choice === 0){
+        return "rock";
+    } else if (choice == 1){
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
